83d84a25b97e6e2faf318933d8daceb241ae2747
Josh Bremer <jbremer@sweetspotdiabetes.com>
2013-12-15 14:52:06 -0800

    Site is looking about 50% in mobile

--- wp-content/themes/bridgetown/home.php
+++ wp-content/themes/bridgetown/home.php
@@ -2,80 +2,43 @@
 
     <div class="container">
 
-			<div id="content" class="row clearfix">
+      <div id="content" class="row clearfix">
 
-						<div id="main" class="col-md-8 clearfix" role="main">
-							<?php 
-							$front_page = get_page_by_title( 'Front Page' );
-							//var_dump($sponsor_page->post_content);
-							?>
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+            <div id="main" class="col-md-8 clearfix" role="main">
+              <p style="text-align:center;">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/bridgetown-logo.png" />
+              </p>
+              <?php
+                $front_page = get_page_by_title( 'Front Page' );
+              ?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-									<section class="entry-content clearfix">
-										<?php echo $front_page->post_content; ?>
-									</section>
-							</article>
+                  <section class="entry-content clearfix">
+                    <?php echo wpautop($front_page->post_content); ?>
+                  </section>
+              </article>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            
+            <?php
+              $sponsor_page = get_page_by_title( 'Front Page Sponsors');
+              //var_dump($sponsor_page->post_content);
+            ?>
+            <div id="sidebar">
+              <div id="sponsor-sidebar">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h1"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									
-
-								</header> <?php // end article header ?>
-
-								<section class="entry-content clearfix">
-									<?php the_content('dddd'); ?>
-								</section> <?php // end article section ?>
-
-								<footer class="article-footer">
-                  <div class="row">
-								  </div>
-                </footer> <?php // end article footer ?>
-
-								<?php // comments_template(); // uncomment if you want to use them ?>
-
-							</article> <?php // end article ?>
-
-							<?php endwhile; ?>
+                <h1>SPONSORED BY:</h1>
+                <div>
+                  <?php echo wpautop($sponsor_page->post_content); ?>
+                </div>
 
 
-                  <?php if (function_exists("emm_paginate")) { ?>
-                      <?php emm_paginate(); ?>
-									<?php } else { ?>
-											<nav class="wp-prev-next">
-													<ul class="clearfix">
-														<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-														<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-													</ul>
-											</nav>
-									<?php } ?>
+              </div>
 
-							<?php endif; ?>
-
-						</div> <?php // end #main ?>
-						<?php 
-							$sponsor_page = get_page_by_title( 'Front Page Sponsors' );
-							//var_dump($sponsor_page->post_content);
-						?>
-						<div id="sidebar" class="pull-right">
-							<div id="sponsor-sidebar">
-
-								<h2>SPONSORED BY:</h2>
-								<div>
-									<?php echo $sponsor_page->post_content; ?>
-								</div>
+            </div>
+            </div> <?php // end #main ?>
 
 
-							</div>
-
-						</div>
-
-
-			</div> <?php // end #content ?>
+      </div> <?php // end #content ?>
 
     </div> <!-- end ./container -->
 
